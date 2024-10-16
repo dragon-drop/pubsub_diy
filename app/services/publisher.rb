c0dffcf7521501d1a45189d0d0f7d8338688ca30
adamdawkins <adam@dragondrop.uk>
2024-10-16 20:55:32 +0100

    Create and publish event records

--- app/services/publisher.rb
+++ app/services/publisher.rb
@@ -1,4 +1,8 @@
 class Publisher
+  def self.record_name(name)
+    @record_name = name
+  end
+
   def self.subscribe(listener, events)
     @subscribers ||= {}
     events.each do |event|
@@ -6,14 +10,20 @@ class Publisher
       @subscribers[event] << listener
     end
   end
-  def self.publish(event_key)
-    # do something to create the event blah blah
-    @subscribers[event_key]&.each do |subscriber|
-      subscriber.send("#{prefix}_#{event_key}")
+  def self.publish(event_key, record:, user_id: nil, event_factory: Event)
+    event = event_factory.create(key: "#{@record_name}_#{event_key}", record: record, user_id: user_id)
+    fetch_subcribers(event_key).each do |subscriber|
+      subscriber.send("#{prefix}_#{event_key}", event)
     end
   end
 
   def self.prefix
     self.name.split("::").map(&:downcase).map { |name| name.gsub("publisher", "") }.join("_")
+  end
+
+  def self.fetch_subcribers(event_key)
+    return [] if @subscribers.nil?
+    return [] if @subscribers[event_key].nil?
+    @subscribers[event_key]
   end
 end
--- spec/services/publisher_spec.rb
+++ spec/services/publisher_spec.rb
@@ -1,29 +1,55 @@
 require_relative "../../app/services/publisher"
 
-module Ops
-  class ProjectPublisher < Publisher
+unless defined?(Ops::ProjectPublisher)
+  module Ops
+    class ProjectPublisher < Publisher
+      record_name :project
+    end
   end
 end
+
 RSpec.describe Publisher do
-  subject(:publisher) { Publisher }
+  subject(:publisher) { Ops::ProjectPublisher }
+
+  # Arguments
+  let(:record) { instance_double("Project") }
+  let(:event_key) { :subscribed_event }
+
+  # Dependencies
+  let(:event_factory) { class_double("Event") }
+  let(:subscriber) { double("Subscriber") }
+
+  # Results
+  let(:event) { instance_double("Event") }
+
+  before do
+    allow(event_factory).to receive(:create).and_return(event)
+    allow(subscriber).to receive(:ops_project_subscribed_event)
+    allow(subscriber).to receive(:ops_project_unsubscribed_event)
+  end
 
   describe '#publish' do
+    subject(:publish) { publisher.publish(event_key, record: record, user_id: 1, event_factory: event_factory) }
+
+    it "creates an event in the database" do
+      publish
+      expect(event_factory).to have_received(:create).with(key: "project_subscribed_event", record:, user_id: 1)
+    end
+
     context "with an event that has been subscribed to" do
-      let(:subscriber) { double("Subscriber") }
       subject(:publisher)  { Ops::ProjectPublisher }
 
-      before do
-        allow(subscriber).to receive(:ops_project_subscribed_event)
-        allow(subscriber).to receive(:ops_project_unsubscribed_event)
-      end
-      it "calls the method on the subscriber based on the current class and module name" do
+      it "calls the method on the subscriber based on the current class and module name with the event" do
         publisher.subscribe(subscriber, [ :subscribed_event ])
-        publisher.publish(:subscribed_event)
-        expect(subscriber).to have_received(:ops_project_subscribed_event)
+        publish
+        expect(subscriber).to have_received(:ops_project_subscribed_event).with(event)
       end
+    end
+    context "without a subscriber to the event" do
+      let(:event_key) { :unsubscribed_event }
 
       it "does not call the method on the subscriber if the event has not been subscribed to" do
-        publisher.publish(:unsubscribed_event)
+        publish
         expect(subscriber).not_to have_received(:ops_project_unsubscribed_event)
       end
     end
